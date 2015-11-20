cae590ccff0890c8d265ec835cf97d087f05354a
naneau <git@naneau.net>
2015-11-20 09:22:22 -0500

    adding support for interpolated messages

--- src/FastNorth/Validator/Constraint/StringLength.php
+++ src/FastNorth/Validator/Constraint/StringLength.php
@@ -19,12 +19,12 @@ class StringLength extends AbstractConstraint
     {
         $min = $this->getOption('min');
         if ($min >= 0 && strlen($value) < $min) {
-            return $this->fail(self::TOO_SHORT);
+            return $this->fail(self::TOO_SHORT, $this->getOptions());
         }
 
         $max = $this->getOption('max');
         if ($max >= 0 && strlen($value) > $max) {
-            return $this->fail(self::TOO_LONG);
+            return $this->fail(self::TOO_LONG, $this->getOptions());
         }
 
         return $this->pass();
