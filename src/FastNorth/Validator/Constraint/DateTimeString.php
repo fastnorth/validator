d27fbcb3e37d8b14e8352b31c680833284757b58
Mike Pirmohamed <mike@tactmarketing.com>
2016-06-22 09:43:49 -0400

    Add a constraint to validate a datetime string

---
+++ src/FastNorth/Validator/Constraint/DateTimeString.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace FastNorth\Validator\Constraint;
+
+/**
+ * DateTimeString
+ *
+ * Must be a valid date and time string
+ */
+class DateTimeString extends AbstractConstraint
+{
+    const INCORRECT_VALUE = 'invalid';
+    const EMPTY_VALUE = 'missing';
+
+    /**
+     * @inheritDoc
+     */
+    public function validate($value, $context = null)
+    {
+        if (empty($value)) {
+            return $this->fail(self::EMPTY_VALUE);
+        }
+
+        if (!is_date($value)) {
+            // is this string valid?
+            return $this->fail(self::INCORRECT_VALUE);
+        }
+
+        $timeStamp = strtotime($value);
+        if (!checkdate(date('m', $timeStamp), date('d', $timeStamp), date('Y', $timeStamp))) {
+            // does this string represent an actual date?
+            return $this->fail(self::INCORRECT_VALUE);
+        }
+
+        return $this->pass();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getDefaultMessages()
+    {
+        return [
+            self::EMPTY_VALUE => 'This field should not be empty',
+            self::INCORRECT_VALUE => 'This field is not a valid date time string'
+        ];
+    }
+}
